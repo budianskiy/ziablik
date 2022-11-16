792a2e72503936e9417156d3bb34b924eee2deb2
budianskiy <117460285+budianskiy@users.noreply.github.com>
2022-11-16 20:30:51 +0200

    blog part 1

---
+++ apps/blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+# Create your models here.
+class BlogCategory(models.Model):
+    name = models.CharField(verbose_name='Имя категории', max_length=255)
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = 'Категория блога'
+        verbose_name_plural = 'Категория блога'
+
+
+class Article(models.Model):
+    category = models.ForeignKey(to=BlogCategory, verbose_name='Категория', on_delete=models.CASCADE)
+    title = models.CharField(verbose_name='Заголовок', max_length=255)
+    text_preview = models.TextField(verbose_name='Текст-превью', null = True, blank = True)
+    text = models.TextField(verbose_name='Текст')
+    publish_date = models.DateTimeField(verbose_name = 'Дата публикации')
+    updated_at = models.DateTimeField(verbose_name='Дата изменения', auto_now = True)
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add = True)
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
