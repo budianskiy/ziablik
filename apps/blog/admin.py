cdd3b3f1094e0ba4b12cf2a0e1507b3c35d90b52
budianskiy <117460285+budianskiy@users.noreply.github.com>
2022-11-18 18:15:32 +0200

    catigories

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from apps.blog.models import BlogCategory, Article
-
+from apps.blog.models import BlogCategory, Article, Tag
 
 admin.site.register(BlogCategory)
 admin.site.register(Article)
+admin.site.register(Tag)
 
