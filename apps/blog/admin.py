87701bb2612b9bfe74d0428936d0aa31400720b8
budianskiy <117460285+budianskiy@users.noreply.github.com>
2022-12-27 16:44:27 +0200

    user articles author

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -23,7 +23,7 @@ class BlogCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'category_link', 'publish_date', 'created_at', 'tag_link']
+    list_display = ['id', 'title', 'category_link', 'user','publish_date', 'created_at', 'tag_link']
     list_display_links = ['id', 'title']
     list_filter = ['category', 'tags']
 
