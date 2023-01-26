ffc47e2870d18d8132e6edfe9b0293c584d7a795
budianskiy <117460285+budianskiy@users.noreply.github.com>
2023-01-26 20:46:33 +0200

    adding metategs and breadcrumbs on all pages

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -6,7 +6,8 @@ from apps.blog.models import BlogCategory, Article, Tag
 
 def blog_category_list(request):
     blog_categories = BlogCategory.objects.all()
-    return render(request, 'blog/category_list.html', {"categories": blog_categories})
+    breadcrumbs = {'current': 'Блог'}
+    return render(request, 'blog/category_list.html', {"categories": blog_categories, 'breadcrumbs': breadcrumbs})
 
 def article_list(request, category_id):
     articles = Article.objects.filter(category_id=category_id)
@@ -16,17 +17,17 @@ def article_list(request, category_id):
         'articles': articles,
         'category': category,
         'breadcrumbs': breadcrumbs,
-        'current': category.name
-
     })
 
 def article_view(request, category_id, article_id):
     category = BlogCategory.objects.get(id=category_id)
     article = Article.objects.get(id=article_id)
-    return render(request, 'blog/article_view.html', {'category': category, 'article' : article})
+    breadcrumbs = {reverse('blog_category_list'): 'Блог', reverse('blog_article_list', kwargs={'category_id': category.id}): category.name, 'current': article.title}
+
+    return render(request, 'blog/article_view.html', {'category': category, 'article': article, 'breadcrumbs': breadcrumbs})
 
 
 def tag_search_view(request, tag_id):
     tag = Tag.objects.get(id=tag_id)
     articles = Article.objects.filter(tags=tag)
-    return render(request, 'blog/tag_search.html', {'tag':tag, 'articles':articles})
+    return render(request, 'blog/tag_search.html', {'tag': tag, 'articles': articles})
