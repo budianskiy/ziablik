915e08ca7e7b444331dccf947105ade43ed8fadc
budianskiy <117460285+budianskiy@users.noreply.github.com>
2023-01-26 21:26:59 +0200

    adding metategs and breadcrumbs on all pages

--- apps/user/views.py
+++ apps/user/views.py
@@ -5,6 +5,7 @@ from apps.user.forms import LoginForm, RegisterForm
 
 
 def user_login(request):
+    breadcrumbs = {'current': "Регистрация"}
     error = None
     if request.method == "POST":
         form = LoginForm(request.POST)
@@ -15,20 +16,24 @@ def user_login(request):
                 login(request, user)
                 return redirect('index')
         error = 'Неправельные логин или пароль'
-    return render(request, 'user/login.html', {'error': error})
+
+    return render(request, 'user/login.html', {'error': error, 'breadcrumbs': breadcrumbs})
 
 
 def user_register(request):
+    breadcrumbs = {'current': "Регистрация"}
     error = None
     if request.method == "POST":
         form = RegisterForm(request.POST, request.FILES)
         if form.is_valid():
+            breadcrumbs = {'current': "Успешная регистрация"}
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password'])
             user.save()
-            return render(request, 'user/welcome.html', {'user': user})
+            return render(request, 'user/welcome.html', {'user': user, 'breadcrumbs': breadcrumbs})
         error = form.errors
-    return render(request, 'user/register.html', {'error': error})
+
+    return render(request, 'user/register.html', {'error': error, 'breadcrumbs': breadcrumbs})
 
 
 def user_logout(request):
