7a1499f928192501f64ae32d2565ed7f7398de08
budianskiy <117460285+budianskiy@users.noreply.github.com>
2023-02-11 12:01:06 +0200

    drf part 1

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'mptt',
     'treebeard',
     'mathfilters',
+    'rest_framework',
+    'rest_framework.authtoken',
 
     'apps.blog',
     'apps.user',
@@ -147,3 +149,13 @@ AUTH_USER_MODEL = 'user.User'
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'index'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
