babc78cfe5ecdd03a7ccafdba09ded4f709243e8
budianskiy <117460285+budianskiy@users.noreply.github.com>
2023-02-11 14:37:06 +0200

    drf homework №1

--- apps/api/catalog/urls.py
+++ apps/api/catalog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from apps.api.catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
-    ProductDeleteView
+    ProductDeleteView, CategoryListView, CategoryDetailView, CategoryCreateView, CategoryUpdateView, CategoryDeleteView
 
 urlpatterns = [
     path('product/', ProductListView.as_view()),
@@ -8,4 +8,10 @@ urlpatterns = [
     path('product/create/', ProductCreateView.as_view()),
     path('product/update/<int:pk>/', ProductUpdateView.as_view()),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view()),
+
+    path('category/', CategoryListView.as_view()),
+    path('category/<int:pk>/', CategoryDetailView.as_view()),
+    path('category/create/', CategoryCreateView.as_view()),
+    path('category/update/<int:pk>/', CategoryUpdateView.as_view()),
+    path('category/delete/<int:pk>/', CategoryDeleteView.as_view()),
 ]
